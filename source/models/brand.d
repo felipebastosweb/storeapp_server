f97854b5d79ce7c9f7be6a67afc85e7a98e79a19
felipe bastos <felipebastosweb@gmail.com>
2024-02-15 20:22:33 -0300

    Update brand.d

--- source/models/brand.d
+++ source/models/brand.d
@@ -1,9 +1,16 @@
 module models.brand;
 
+import vibe.d;
+import vibe.data.serialization;
+import vibe.db.mongo.mongo;
+
+
 struct Brand {
+    BsonObjectID _id;
     string name;
     string address;
     string phone;
     string site;
     string email;
+    @optional Product[int] products;
 }
