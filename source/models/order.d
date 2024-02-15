553f7f202713762d4ebc8c5c1a9d301ec3e63536
felipe bastos <felipebastosweb@gmail.com>
2024-02-15 20:24:31 -0300

    Update order.d

--- source/models/order.d
+++ source/models/order.d
@@ -2,12 +2,27 @@ module models.order;
 
 import std.datetime;
 
+import vibe.d;
+import vibe.data.serialization;
+import vibe.db.mongo.mongo;
+
 import models.shop:Shop;
 import models.customer:Customer;
 
 struct Order {
+    BsonObjectID _id;
     Shop shop;
     Customer customer;
     double total;
     DateTime request_date;
+    @optional Order[int] orders;
+}
+
+struct OrderItem {
+    BsonObjectID _id;
+    Order order;
+    double quantity;
+    double price;
+    double total;
+    
 }
