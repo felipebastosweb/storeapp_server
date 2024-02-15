0bca3e57d0d27d7ca09c826d1f8dc54c5247bfaa
felipe bastos <felipebastosweb@gmail.com>
2024-02-15 20:19:28 -0300

    Update supplier.d

--- source/models/supplier.d
+++ source/models/supplier.d
@@ -1,12 +1,17 @@
 module models.supplier;
 
+import vibe.d;
+import vibe.data.serialization;
+import vibe.db.mongo.mongo;
+
 import models.purchase:Purchase;
 
 struct Supplier {
+    BsonObjectID _id;
     string name;
     string address;
     string phone;
     string site;
     string email;
-    Purchase[int] purchases;
+    @optional Purchase[int] purchases;
 }
