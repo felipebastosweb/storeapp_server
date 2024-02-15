18cbb0b486030819b4d11f1803eafbc2fa4bf000
felipe bastos <felipebastosweb@gmail.com>
2024-02-15 20:30:35 -0300

    Update address.d

--- source/models/address.d
+++ source/models/address.d
@@ -1,2 +1,12 @@
 module models.address;
 
+import vibe.d;
+import vibe.data.serialization;
+import vibe.db.mongo.mongo;
+
+struct Address {
+    BsonObjectID _id;
+    string location;
+    string latitude;
+    string longitude;
+}
