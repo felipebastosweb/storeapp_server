10cd36b1d0e0c42e52ffae87f080f85e60d17396
felipe bastos <felipebastosweb@gmail.com>
2024-02-15 17:46:34 -0300

    Update product.d

--- source/controllers/product.d
+++ source/controllers/product.d
@@ -1,5 +1,66 @@
 module controllers.product;
 
+import std.stdio;
+// Vibed
+import vibe.d;
+import vibe.db.mongo.mongo;
+
+import std.algorithm: map;
+
+import database;
+import models.customer;
+
+import translation;
+
 class ProductController {
+    private MongoClient client;
+	MongoCollection coll;
+
+	this() {
+		client = connectMongoDB("127.0.0.1");
+		coll = client.getCollection("storeapp.products");
+	}
     
+	// GET /
+	@method(HTTPMethod.GET)
+	@path("/products")
+	void index()
+	{
+		/*
+		bool authenticated = ms_authenticated;
+		render!("user/index.dt", authenticated);
+		*/
+		auto products = coll.find().map!(bson => deserializeBson!Product(bson));
+		render!("products_index.dt", products);
+	}
+	
+
+	// GET /users/:usernameeeeeeeee
+    @method(HTTPMethod.GET)
+	@path("/products/:slug")
+    void show(HTTPServerRequest req, HTTPServerResponse res)
+    {
+		struct Q { string slug; }
+        auto productNullable = coll.findOne!Customer(Q(req.params["slug"]));
+		if (! productNullable.isNull) {
+			// Acessar os campos da estrutura Customer
+			auto product = productNullable.get;
+			render!("products_show.dt", product);
+		} else {
+
+		}
+    }
+	
+	// GET /
+	@method(HTTPMethod.GET)
+	@path("/products/new")
+	void new_form()
+	{
+		/*
+		bool authenticated = ms_authenticated;
+		render!("user/index.dt", authenticated);
+		*/
+        auto product = Product();
+		render!("products_new.dt", product);
+	}
 }
