02102acb3a807b1bf85fc0e6d624b146538534fc
felipe bastos <felipebastosweb@gmail.com>
2024-02-15 17:20:09 -0300

    Update supplier.d

--- source/controllers/supplier.d
+++ source/controllers/supplier.d
@@ -1,5 +1,49 @@
 module controllers.supplier;
 
+import std.stdio;
+// Vibed
+import vibe.d;
+import vibe.db.mongo.mongo;
+
+import std.algorithm: map;
+
+import database;
+import models.supplier;
+
+import translation;
+
 class SupplierController {
+    private MongoClient client;
+	MongoCollection coll;
+
+	this() {
+		client = connectMongoDB("127.0.0.1");
+		coll = client.getCollection("storeapp.suppliers");
+	}
     
+	// GET /
+	@method(HTTPMethod.GET)
+	@path("/suppliers")
+	void index()
+	{
+		/*
+		bool authenticated = ms_authenticated;
+		render!("user/index.dt", authenticated);
+		*/
+		auto suppliers = coll.find().map!(bson => deserializeBson!Supplier(bson));
+		render!("suppliers_index.dt", suppliers);
+	}
+
+    
+	// GET /
+	@method(HTTPMethod.GET)
+	@path("/suppliers/new")
+	void new_form()
+	{
+		/*
+		bool authenticated = ms_authenticated;
+		render!("shops/index.dt", authenticated);
+		*/
+		render!("suppliers_new.dt");
+	}
 }
