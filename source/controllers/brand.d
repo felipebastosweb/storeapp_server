1e070153b0be5fdb86de6cc1f747d50bb32a9aab
felipe bastos <felipebastosweb@gmail.com>
2024-02-20 18:18:04 -0300

    Update brand.d

--- source/controllers/brand.d
+++ source/controllers/brand.d
@@ -21,7 +21,7 @@ class BrandController {
 		coll = client.getCollection("storeapp.brands");
 	}
     
-	// GET /
+	// GET /brands
 	@method(HTTPMethod.GET)
 	@path("/brands")
 	void index()
@@ -35,13 +35,13 @@ class BrandController {
 	}
 	
 
-	// GET /users/:usernameeeeeeeee
+	// GET /brands/:_id
     @method(HTTPMethod.GET)
-	@path("/brands/:slug")
+	@path("/brands/:_id")
     void show(HTTPServerRequest req, HTTPServerResponse res)
     {
-		struct Q { string slug; }
-        auto brandNullable = coll.findOne!Brand(Q(req.params["slug"]));
+		struct Q { BsonObjectID _id = BsonObjectID.fromString(req.params["_id"]); }
+        auto brandNullable = coll.findOne!Brand(Q());
 		if (! brandNullable.isNull) {
 			// Acessar os campos da estrutura Customer
 			auto brand = brandNullable.get;
@@ -51,7 +51,7 @@ class BrandController {
 		}
     }
 	
-	// GET /
+	// GET /brands/new
 	@method(HTTPMethod.GET)
 	@path("/brands/new")
 	void new_form()
@@ -64,6 +64,7 @@ class BrandController {
 		render!("brands_new.dt", brand);
 	}
 
+	// POST /brands
     @method(HTTPMethod.POST)
 	@path("/brands")
 	void create(HTTPServerRequest req, HTTPServerResponse res)
@@ -71,11 +72,53 @@ class BrandController {
 		Brand brand;
 		brand._id = BsonObjectID.generate; // Gera um ID aleatório para o usuário
 		brand.name = req.form["name"];
+		brand.description = req.form["description"];
+		brand.address = req.form["address"];
 		brand.email = req.form["email"];
 		brand.site = req.form["site"];
 		brand.phone1 = req.form["phone1"];
 		brand.phone2 = req.form["phone2"];
 		coll.insertOne(brand);
+        res.redirect("/brands");
+	}
+    
+	// GET /brands/:_id/edit
+	@method(HTTPMethod.GET)
+	@path("/brands/:_id/edit")
+	void edit_form(HTTPServerRequest req, HTTPServerResponse res)
+	{
+		/*
+		bool authenticated = ms_authenticated;
+		render!("brand/index.dt", authenticated);
+		*/
+		struct Q { BsonObjectID _id = BsonObjectID.fromString(req.params["_id"]); }
+        auto brandNullable = coll.findOne!Brand(Q());
+		if (! brandNullable.isNull) {
+			// Acessar os campos da estrutura Brand
+			Brand brand = brandNullable.get;
+			render!("brands_edit.dt", brand);
+		}
+	}
+
+    // POST /brands/:_id
+	@method(HTTPMethod.POST)
+	@path("/brands/:_id")
+	void change(HTTPServerRequest req, HTTPServerResponse res)
+	{
+		auto _id = BsonObjectID.fromString(req.params["_id"]);
+        // filter
+		BsonObjectID[string] filter;
+		filter["_id"] = _id;
+        // update
+		Bson[string][string] update;
+		update["$set"]["name"] = req.form["name"];
+		update["$set"]["description"] = req.form["description"];
+		update["$set"]["address"] = req.form["address"];
+		update["$set"]["site"] = req.form["site"];
+		update["$set"]["email"] = req.form["email"];
+		update["$set"]["phone1"] = req.form["phone1"];
+		update["$set"]["phone2"] = req.form["phone2"];
+		coll.updateOne(filter, update);
         res.redirect("/brands");
 	}
     
