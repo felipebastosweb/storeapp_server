e398757eaae77c8e38f72e9c50922680f76146c0
felipe bastos <felipebastosweb@gmail.com>
2024-02-22 12:07:03 -0300

    Cadastro de compras sem itens

--- source/controllers/purchase.d
+++ source/controllers/purchase.d
@@ -5,6 +5,7 @@ import std.stdio;
 import vibe.d;
 import vibe.db.mongo.mongo;
 
+import std.conv;
 import std.algorithm: map;
 
 import database;
@@ -40,22 +41,41 @@ class PurchaseController {
 		render!("purchases_index.dt", purchases);
 	}
 	
-	// GET /
+	// GET /purchases/new
 	@method(HTTPMethod.GET)
 	@path("/purchases/new")
 	void new_form()
 	{
 		/*
 		bool authenticated = ms_authenticated;
-		render!("user/index.dt", authenticated);
+		render!("purchases/index.dt", authenticated);
 		*/
         auto purchase = Purchase();
 		auto shops = coll_shop.find().map!(bson => deserializeBson!Shop(bson));
 		auto suppliers = coll_supplier.find().map!(bson => deserializeBson!Supplier(bson));
 		render!("purchases_new.dt", purchase, shops, suppliers);
 	}
-	
 
+	// POST /purchases
+    @method(HTTPMethod.POST)
+	@path("/purchases")
+	void create(HTTPServerRequest req, HTTPServerResponse res)
+	{
+		struct Q { BsonObjectID _id; }
+		Purchase purchase;
+		purchase._id = BsonObjectID.generate; // Gera um ID aleatório para o usuário
+		purchase.shop_id = req.form["shop_id"];
+		purchase.shop = coll_shop.findOne!Shop(Q(BsonObjectID.fromString(purchase.shop_id))).get;
+		purchase.supplier_id = req.form["supplier_id"];
+		purchase.supplier = coll_supplier.findOne!Supplier(Q(BsonObjectID.fromString(purchase.supplier_id))).get;
+		purchase.value = to!double(req.form["value"]);
+		purchase.taxes = to!double(req.form["taxes"]);
+		purchase.total = to!double(req.form["total"]);
+		purchase.purchase_date = Date.fromISOExtString(req.form["purchase_date"]);
+		coll.insertOne(purchase);
+        res.redirect("/purchases");
+	}
+	
 	// GET /users/:_id
     @method(HTTPMethod.GET)
 	@path("/purchases/:_id")
